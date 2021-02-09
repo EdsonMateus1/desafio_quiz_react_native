b28126d200be3113db7003f29ab794c487d837f0
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2021-02-09 16:22:02 -0300

    imagens

--- utils/quest_array.ts
+++ utils/quest_array.ts
@@ -1,7 +1,7 @@
 export const quest = [
   {
     quest: "1.De quem é a famosa frase “Penso, logo existo”?",
-    image: require("./assets/undraw_Publish_article_re_3x8h.png"),
+    image: require("../assets/undraw_Publish_article_re_3x8h.png"),
     correct: "Descartes",
     answers: [
       "a) Platão",
@@ -13,7 +13,7 @@ export const quest = [
   },
   {
     quest: "2.De onde é a invenção do chuveiro elétrico?",
-    image: require("./assets/undraw_Code_thinking_re_gka2.png"),
+    image: require("../assets/undraw_Code_thinking_re_gka2.png"),
     correct: "Brasil",
     answers: [
       "a) França",
@@ -26,7 +26,7 @@ export const quest = [
   {
     quest:
       "3.Qual o nome do presidente do Brasil que ficou conhecido como Jango?",
-    image: require("./assets/undraw_Code_thinking_re_gka2.png"),
+    image: require("../assets/undraw_Personal_goals_re_iow7.png"),
     correct: "João Goulart",
     answers: [
       "a) Jânio Quadros",
@@ -38,7 +38,7 @@ export const quest = [
   },
   {
     quest: "4.Qual o livro mais vendido no mundo a seguir à Bíblia?",
-    image: require("./assets/undraw_Code_thinking_re_gka2.png"),
+    image: require("../assets/undraw_Location_search_re_ttoj.png"),
     correct: "Dom Quixote",
     answers: [
       "a) O Senhor dos Anéis",
@@ -50,7 +50,7 @@ export const quest = [
   },
   {
     quest: "5.Quantas casas decimais tem o número pi?",
-    image: require("./assets/undraw_Code_thinking_re_gka2.png"),
+    image: require("../assets/undraw_File_searching_re_3evy.png"),
     correct: "Infinitas",
     answers: [
       "a) Duas",
@@ -63,13 +63,13 @@ export const quest = [
   {
     quest:
       "6.Atualmente, quantos elementos químicos a tabela periódica possui?",
-    image: require("./assets/undraw_Code_thinking_re_gka2.png"),
+    image: require("../assets/undraw_Devices_re_dxae.png"),
     correct: "118",
     answers: ["a) 113", "b) 109", "c) 108", "e) 92", "d) 118"],
   },
   {
     quest: "7.O que a palavra legend significa em português?",
-    image: require("./assets/undraw_Code_thinking_re_gka2.png"),
+    image: require("../assets/undraw_Notify_re_65on.png"),
     correct: "Lenda",
     answers: [
       "a) Legenda",
@@ -81,13 +81,13 @@ export const quest = [
   },
   {
     quest: "8.Qual o número mínimo de jogadores numa partida de futebol?",
-    image: require("./assets/undraw_Code_thinking_re_gka2.png"),
+    image: require("../assets/undraw_Just_saying_re_kw9c.png"),
     correct: "7",
     answers: ["a) 8", "b) 10", "c) 9", "d) 5", "e) 7"],
   },
   {
     quest: "9.Quem é o autor de “O Príncipe”?",
-    image: require("./assets/undraw_Code_thinking_re_gka2.png"),
+    image: require("../assets/undraw_environmental_study_skau.png"),
     correct: "Maquiavel",
     answers: [
       "a) Maquiavel",
@@ -100,7 +100,7 @@ export const quest = [
 
   {
     quest: "10.Quanto tempo a luz do Sol demora para chegar à Terra?",
-    image: require("./assets/undraw_Code_thinking_re_gka2.png"),
+    image: require("../assets/undraw_real_time_collaboration_c62i.png"),
     correct: "8 minutos",
     answers: [
       "a) 12 minutos",
@@ -112,7 +112,7 @@ export const quest = [
   },
   {
     quest: "11.Qual a nacionalidade de Che Guevara?",
-    image: require("./assets/undraw_Code_thinking_re_gka2.png"),
+    image: require("../assets/undraw_social_bio_8pql.png"),
     correct: "Argentina",
     answers: [
       "a) Cubana",
@@ -125,7 +125,7 @@ export const quest = [
   {
     quest:
       "12.Qual personagem folclórico costuma ser agradado pelos caçadores com a oferta de fumo?",
-    image: require("./assets/undraw_Code_thinking_re_gka2.png"),
+    image: require("../assets/undraw_mobile_posts_tlwg.png"),
     correct: "Caipora",
     answers: [
       "a) Caipora",
@@ -137,7 +137,7 @@ export const quest = [
   },
   {
     quest: "13.Qual a montanha mais alta do Brasil?",
-    image: require("./assets/undraw_Code_thinking_re_gka2.png"),
+    image: require("../assets/undraw_our_solution_htvp.png"),
     correct: "Pico da Neblina",
     answers: [
       "a) Pico da Neblina",
@@ -149,7 +149,7 @@ export const quest = [
   },
   {
     quest: "14.Qual destes países é transcontinental?",
-    image: require("./assets/undraw_Code_thinking_re_gka2.png"),
+    image: require("../assets/undraw_tutorial_video_vtd1.png"),
     correct: "Rússia",
     answers: [
       "a) Rússia",
@@ -161,7 +161,7 @@ export const quest = [
   },
   {
     quest: "15.Em qual local da Ásia o português é língua oficial?",
-    image: require("./assets/undraw_Code_thinking_re_gka2.png"),
+    image: require("../assets/undraw_dev_focus_b9xo.png"),
     correct: "Macau",
     answers: [
       "a) Índia",
